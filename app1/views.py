49221061d07b5d213ce42bce99522f4e7e5553c8
Bishwo_Bijaya_Shah <bishwo-bijaya.shah@stud.uni-bamberg.de>
2023-12-28 18:58:54 +0100

    Add 'app1' to INSTALLED_APPS in settings.py and configure urls and views

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    message = "Happy Dockerizing!!"
+    return render(request, 'index.html', {"message": message})
